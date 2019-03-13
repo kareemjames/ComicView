81dd843ff3c60a64940048b92f147d2cbfcae6dc
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-03-13 15:02:28 -0400

    create search by name method, did not test yet

--- src/main/java/com/detroitlabs/comicview/controller/ComicController.java
+++ src/main/java/com/detroitlabs/comicview/controller/ComicController.java
@@ -5,7 +5,9 @@ import com.detroitlabs.comicview.model.ComicWrapper;
 import com.detroitlabs.comicview.service.ComicService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
@@ -23,6 +25,15 @@ public class ComicController {
         ComicWrapper cw = comicService.fetchAllData();
         List<Character> allCharacters = cw.getResults();
         return allCharacters.toString();
+    }
+
+    @RequestMapping("search")
+    public String searchByCharacterName(@RequestParam("q") String searchValue, ModelMap modelMap) {
+        comicService.setSearchName(searchValue);
+        ComicWrapper cw = comicService.fetchbyName();
+        List<Character> allCharacters = cw.getResults();
+        modelMap.put("allCharacters", allCharacters);
+        return"index";
     }
 
 
